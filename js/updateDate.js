b4d764a9c246efabba88df7c7b5fdc342dcd942e
jneal43 <nea17003@byui.edu>
2022-01-13 15:09:45 -0700

    dateTime complete

--- js/updateDate.js
+++ js/updateDate.js
@@ -1,2 +1,4 @@
-//const options = {weekday: 'numeric', }
-document.getElementById('updateDate').textContent = new Date(document.lastModified).toLocaleDateString("en-US");
+var date = new Date(document.lastModified).toLocaleDateString("en-US");
+var time = new Date(document.lastModified).toLocaleTimeString('en-US', { hour12: false });
+var dateTime = date +' '+time;
+document.getElementById('updateDate').textContent = dateTime
