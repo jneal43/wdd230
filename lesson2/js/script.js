d0adcf223ac170cacce56e53edf590275b49221f
jneal43 <nea17003@byui.edu>
2022-01-14 23:04:18 -0700

    Create lesson2 folder, html, css, js documents, media query complete(ish), pending images

---
+++ lesson2/js/script.js
@@ -0,0 +1,8 @@
+var space = new Date();
+let year = space.getFullYear();
+document.getElementById('year').textContent = year;
+
+var date = new Date(document.lastModified).toLocaleDateString("en-US");
+var time = new Date(document.lastModified).toLocaleTimeString('en-US', { hour12: false });
+var dateTime = date +' '+time;
+document.getElementById('updateDate').textContent = dateTime;
