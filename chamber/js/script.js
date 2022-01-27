5001d98ca084b45bdfd9796c0a3198b6f5349b34
jneal43 <nea17003@byui.edu>
2022-01-26 23:55:08 -0700

    got the dates working nicely and reworked how the script is organized

---
+++ chamber/js/script.js
@@ -0,0 +1,23 @@
+var currentDate = new Date();
+
+let weekDay = new Intl.DateTimeFormat('en-US', {weekday:'long'}).format(currentDate);
+
+let monthDay = currentDate.getDate();
+
+let month = new Intl.DateTimeFormat('en-US', {month:'long'}).format(currentDate);
+
+let year = currentDate.getFullYear();
+
+var date = new Date(document.lastModified).toLocaleDateString("en-US");
+
+var time = new Date(document.lastModified).toLocaleTimeString('en-US', { hour12: false });
+
+var longDate = weekDay +', '+ monthDay +' '+ month +' '+ year;
+
+var dateTime = date +' '+ time;
+
+document.getElementById('longDate').textContent = longDate;
+
+document.getElementById('year').textContent = year;
+
+document.getElementById('updateDate').textContent = dateTime;
