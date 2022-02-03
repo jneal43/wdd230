3bd02ed7fa25aa677cbd48a785b4b36280b125f2
jneal43 <nea17003@byui.edu>
2022-02-02 19:01:33 -0700

    working JS for list

---
+++ practice/js/bom.js
@@ -0,0 +1,26 @@
+const button = document.querySelector('.submit');
+const input = document.getElementById('favchap').value;
+const list = document.querySelector('.list');
+
+button.addEventListener('click', addToList);
+
+function addToList() {
+    let newItem = document.getElementById('favchap').value;
+    document.getElementById('favchap').value = '';
+    const listItem = document.createElement('li');
+    const span = document.createElement('span');
+    const del_button = document.createElement('button');
+    list.append(span);
+    list.append(del_button);
+    list.appendChild(listItem);
+    span.textContent = newItem;
+    del_button.textContent = '❌';
+    del_button.addEventListener('click', deleteFromList);
+
+    function deleteFromList() {
+        listItem.classList.toggle('disappear');
+        span.classList.toggle('disappear');
+        del_button.classList.toggle('disappear');
+    }
+
+}
