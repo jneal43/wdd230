6e2795f7688d569c0463cd165b5cf100be2a3aad
jneal43 <nea17003@byui.edu>
2022-02-09 20:19:35 -0700

    made the windchill thing work, go team

---
+++ chamber/js/windchill.js
@@ -0,0 +1,14 @@
+let temperature = 30;
+let windSpeed = document.getElementById('windSpeed').textContent;
+
+if (temperature <= 50 && windSpeed > 3) {
+    var rawWindChill = 35.74 + (.6215 * temperature) - (35.75 * (windSpeed ** .16)) + (.4275 * temperature * (windSpeed ** .16));
+    console.log("Raw: " + rawWindChill + " Degrees");
+    var sanitaryWindChill = Math.round(rawWindChill * 10);
+    console.log("Sanitary: " + sanitaryWindChill / 10);
+} else {
+    console.log("N/A");
+}
+
+
+document.getElementById('windChill').textContent = "Wind Chill: " + (sanitaryWindChill / 10) + "°F";
