698342d8339470dac014783d57af18ae7ad56f44
jneal43 <nea17003@byui.edu>
2022-02-09 20:19:04 -0700

    formatting

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,30 +1,18 @@
 var currentDate = new Date();
-
 let weekDay = new Intl.DateTimeFormat('en-US', {weekday:'long'}).format(currentDate);
-
 let monthDay = currentDate.getDate();
-
 let month = new Intl.DateTimeFormat('en-US', {month:'long'}).format(currentDate);
-
 let year = currentDate.getFullYear();
-
 var date = new Date(document.lastModified).toLocaleDateString("en-US");
-
 var time = new Date(document.lastModified).toLocaleTimeString('en-US', { hour12: false });
-
 var longDate = weekDay +', '+ monthDay +' '+ month +' '+ year;
-
 var dateTime = date +' '+ time;
+const nav = document.querySelector('.navigation')
 
 document.getElementById('long-date').textContent = longDate;
-
 document.getElementById('year').textContent = year;
-
 document.getElementById('updateDate').textContent = dateTime;
-
 document.getElementById('ham').addEventListener('click', toggleMenu)
-
-const nav = document.querySelector('.navigation')
 
 function toggleMenu() {
     nav.classList.toggle('responsive');
