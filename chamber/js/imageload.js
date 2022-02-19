128ce6d9f9c0abe514a2fcbaa6b8b9e0680c4960
jneal43 <nea17003@byui.edu>
2022-02-18 20:21:52 -0700

    relocated imageload.js

---
+++ chamber/js/imageload.js
@@ -0,0 +1,25 @@
+const images = document.querySelectorAll('[data-src]');
+const options = {    threshold: [.5]    };
+
+function preloadImage(img) {
+    img.src = img.getAttribute('data-src');
+}
+
+const io = new IntersectionObserver (
+    (entries, io) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                preloadImage(entry.target); //call a function, send in the image that is currently intersecting
+                io.unobserve(entry.target); //stop observing once loaded
+            } else {
+                return;
+            }
+            console.log(entries);
+        })
+        console.log(entries);
+    }, options
+);
+
+images.forEach(image => {
+    io.observe(image);
+});
