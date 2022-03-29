642f05a59584e65cc0be129d0c55f68bdc7ea7a0
jneal43 <nea17003@byui.edu>
2022-03-28 20:32:11 -0600

    added some content for experiments, including a json file and modified directory JS to display the cards, some images as well

---
+++ final/js/experiments.js
@@ -0,0 +1,35 @@
+const json = 'js/data.json';
+const cards = document.querySelector('.cards');
+
+fetch(json)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+        const experiments = jsonObject['experiments'];
+        if (window.innerWidth < 760 || window.innerWidth > 1140) {
+            experiments.forEach(displayCards);
+        }
+    });
+
+function displayCards(experiment) {
+    let card = document.createElement('section');
+    let name = document.createElement('h2');
+    let description = document.createElement('p');
+    let img = document.createElement('img');
+
+    name.textContent = `${experiment.name}`;
+    description.textContent = `${experiment.description}`;
+
+    card.setAttribute('class', `card`);
+    img.setAttribute('src', experiment.image);
+    img.setAttribute('id', `image`);
+    img.setAttribute('alt', `The logo for ${experiment.name}`);
+
+    card.appendChild(name);
+    card.appendChild(img);
+    card.appendChild(description);
+
+    document.querySelector('div.cards').appendChild(card);
+}
